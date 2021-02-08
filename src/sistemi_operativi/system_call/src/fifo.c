74a5fb6ae1c586056efa6168e868e0e345158ff6
Luca <45152996+lbrame@users.noreply.github.com>
2021-02-08 09:45:22 +0100

    Build system: Improved CMake setup

---
+++ src/sistemi_operativi/system_call/src/fifo.c
@@ -0,0 +1,6 @@
+/// @file fifo.c
+/// @brief Contiene l'implementazione delle funzioni
+///         specifiche per la gestione delle FIFO.
+
+#include "err_exit.h"
+#include "fifo.h"
---
+++ src/sistemi_operativi/system_call/src/pipe.h
@@ -0,0 +1,5 @@
+/// @file pipe.h
+/// @brief Contiene la definizioni di variabili e
+///         funzioni specifiche per la gestione delle PIPE.
+
+#pragma once
