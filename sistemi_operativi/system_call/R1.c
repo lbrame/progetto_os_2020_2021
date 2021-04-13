7c863b69b28384f2f189a48dcdf3cc0d47e0da3f
Levent Bekdemir <lorenzo.bonanni@studenti.univr.it>
2021-04-13 11:41:20 +0200

    Modified CmakeLists, now working and modified file headers according to teacher new specifications

--- sistemi_operativi/system_call/R1.c
+++ sistemi_operativi/system_call/R1.c
@@ -2,7 +2,7 @@
 #include "unistd.h"
 
 int main(int argc, char * argv[]) {
-    char data[] = "Id;Message;Id_Sender;Id_Receiver;Time_arrival;Time_departure";
+    char data[] = "ID;Message;IDSender;IDReceiver;TimeArrival;TimeDeparture";
     write_file("OutputFiles/F6.csv", data);
     sleep(1);
     return 0;
