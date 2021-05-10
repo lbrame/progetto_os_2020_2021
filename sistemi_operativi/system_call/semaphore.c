a751e39612746ad0717fc21bb569fa9607798c09
Luca <45152996+lbrame@users.noreply.github.com>
2021-05-10 18:46:37 +0200

    Update CMakeLists.txt to include semaphores, initialize semaphore.h header file with semun union, add semOp wrapper function to semaphore.c

--- sistemi_operativi/system_call/semaphore.c
+++ sistemi_operativi/system_call/semaphore.c
@@ -4,3 +4,16 @@
 
 #include "err_exit.h"
 #include "semaphore.h"
+
+/**
+ * Wrapper function to run semop() on a specific semaphore more easily.
+ * @param semid - id of the semaphore array
+ * @param sem_num - semaphore number within the semid array
+ * @param sem_op - operation to run on the selected semaphore
+ */
+void semOp(int semid, unsigned short sem_num, short sem_op) {
+    struct sembuf sop = {.sem_num = sem_num, .sem_op = sem_op, .sem_flg = 0};
+
+    if (semop(semid, &sop, 1) != -1)
+        ErrExit("semop failed in semOp wrapper");
+}
--- sistemi_operativi/system_call/semaphore.h
+++ sistemi_operativi/system_call/semaphore.h
@@ -3,3 +3,16 @@
 ///         specifiche per la gestione dei SEMAFORI.
 
 #pragma once
+
+#ifndef SEMUN_H
+#define SEMUN_H
+#include <sys/sem.h>
+
+// definition of the union semun
+union semun {
+    int val;
+    struct semid_ds * buf;
+    unsigned short * array;
+};
+
+#endif
