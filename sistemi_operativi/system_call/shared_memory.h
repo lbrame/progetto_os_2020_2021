dcea2a5892b5953523e3ce5fa8da01c09f669a93
Luca <45152996+lbrame@users.noreply.github.com>
2021-05-17 23:29:04 +0200

    Implement shared memory wrapper functions

--- sistemi_operativi/system_call/shared_memory.h
+++ sistemi_operativi/system_call/shared_memory.h
@@ -2,4 +2,30 @@
 /// @brief Contiene la definizioni di variabili e funzioni
 ///         specifiche per la gestione della MEMORIA CONDIVISA.
 
+#include "err_exit.h"
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+
 #pragma once
+
+/**
+ * Generate shared memory area.
+ * @param size size of the shared memory area to initialize
+ * @return id of the shared memory
+ */
+int generate_shmem(size_t size);
+
+/**
+ * Remove shared memory area.
+ * @param shmid id of the shared memory area to remove
+ */
+void destroy_shmem(int shmid);
+
+/**
+ * Attach current process to shared memory area.
+ * @param shmid id of existing shared memory area to attach to
+ * @return pointer where shared memory is attached
+ */
+int* attach_smem(int* shmid);
