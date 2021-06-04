6781d679bae31a7f1760edd69eee226d92ef5d08
Lorenzo Bonanni <lorenzo.bonanni@studenti.univr.it>
2021-06-04 11:38:25 +0200

    Read and Parsed files F8.csv and F9.csv

--- sistemi_operativi/system_call/hackler.c
+++ sistemi_operativi/system_call/hackler.c
@@ -2,10 +2,8 @@
 /// @brief Contiene l'implementazione del client.
 
 #include "err_exit.h"
-#include <malloc.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <stdlib.h>
 #include "defines.h"
 
@@ -16,6 +14,11 @@ typedef struct {
     char action[13];
 } hackler_struct;
 
+typedef struct {
+    char senderID[2];
+    pid_t pid;
+} pid_struct;
+
 
 /**
  * parse the read text to create a list of structures
@@ -23,20 +26,23 @@ typedef struct {
  * @param fileSize the size of the buffer
  * @return a pointer to a list of hackler_struct
  */
-hackler_struct *parse_file(char *inputBuffer, int message_number) {
-    hackler_struct *messages = malloc(message_number * (int)sizeof(hackler_struct));
+hackler_struct *parse_hackler_struct(char *inputBuffer, int message_number) {
+    hackler_struct *messages = malloc(message_number * (int) sizeof(hackler_struct));
     char *row_context;
     char *field_context;
     int row_counter = 0;
 
     // iterate over the rows
-    for (char *row_token = strtok_r(inputBuffer, "\n", &row_context); row_token; row_token = strtok_r(NULL, "\n", &row_context)) {
+    for (char *row_token = strtok_r(inputBuffer, "\n", &row_context); row_token; row_token = strtok_r(NULL, "\n",
+                                                                                                      &row_context)) {
         // row_counter at 0 is the first row which contains the field names
         if (row_counter != 0) {
             int field_counter = 0;
             hackler_struct *message = malloc(sizeof(hackler_struct));
             // iterate over the fields
-            for (char *field_token = strtok_r(row_token, ";", &field_context); field_token; field_token = strtok_r(NULL, ";", &field_context)) {
+            for (char *field_token = strtok_r(row_token, ";", &field_context); field_token; field_token = strtok_r(NULL,
+                                                                                                                   ";",
+                                                                                                                   &field_context)) {
                 switch (field_counter) {
                     case 0:
                         message->id = atoi(field_token);
@@ -55,12 +61,51 @@ hackler_struct *parse_file(char *inputBuffer, int message_number) {
                 }
                 field_counter++;
             }
-            messages[row_counter-1] = *message;
+            messages[row_counter - 1] = *message;
         }
         row_counter++;
     }
     return messages;
 }
+
+
+pid_struct *parse_pid_struct(char *inputBuffer, int message_number) {
+    pid_struct *messages = malloc(message_number * (int) sizeof(pid_struct));
+    char *row_context;
+    char *field_context;
+    int row_counter = 0;
+
+    // iterate over the rows
+    for (char *row_token = strtok_r(inputBuffer, "\n", &row_context); row_token; row_token = strtok_r(NULL, "\n",
+                                                                                                      &row_context)) {
+        // row_counter at 0 is the first row which contains the field names
+        if (row_counter != 0) {
+            int field_counter = 0;
+            pid_struct *message = malloc(sizeof(pid_struct));
+            // iterate over the fields
+            for (char *field_token = strtok_r(row_token, ";", &field_context); field_token; field_token = strtok_r(NULL,
+                                                                                                                   ";",
+                                                                                                                   &field_context)) {
+                switch (field_counter) {
+                    case 0:
+                        memcpy(message->senderID, field_token, sizeof(&field_token));
+                        break;
+                    case 1:
+                        message->pid = atoi(field_token);
+                        break;
+                    default:
+                        ErrExit("parse_file");
+                }
+                field_counter++;
+            }
+            messages[row_counter - 1] = *message;
+        }
+        row_counter++;
+    }
+    return messages;
+}
+
+
 
 /**
  * join all messages preparing the text to be outputted to file
@@ -136,6 +181,7 @@ hackler_struct *parse_file(char *inputBuffer, int message_number) {
 }*/
 
 int main(int argc, char *argv[]) {
+    // READ F7.csv
     // argv[1] is the relative path to the input file and it is passes as a keyboard argument
     int fileSize = get_file_size(argv[1]);
     // allocate buffer to read file of size fileSize + 1(space for /0)
@@ -145,11 +191,39 @@ int main(int argc, char *argv[]) {
     }
     read_file(inputBuffer, argv[1], fileSize);
     int message_number = count_messages(inputBuffer, fileSize);
-    hackler_struct* messages = parse_file(inputBuffer, message_number);
+    hackler_struct *messages = parse_hackler_struct(inputBuffer, message_number);
     free(inputBuffer);
 
 
+    // READ f8.csv
+    char path_sender[] = "OutputFiles/F8.csv";
+    fileSize = get_file_size(path_sender);
+    // allocate buffer to read file of size fileSize + 1(space for /0)
+    inputBuffer = malloc(sizeof(char) * fileSize + 1);
+    if (inputBuffer == NULL)
+        ErrExit("malloc");
+
+    read_file(inputBuffer, path_sender, fileSize);
+    message_number = count_messages(inputBuffer, fileSize);
+    pid_struct *sender_messages = parse_pid_struct(inputBuffer, message_number);
+    free(inputBuffer);
+
+    // READ f9.csv
+    char path_receiver[] = "OutputFiles/F9.csv";
+    fileSize = get_file_size(path_receiver);
+    // allocate buffer to read file of size fileSize + 1(space for /0)
+    inputBuffer = malloc(sizeof(char) * fileSize + 1);
+    if (inputBuffer == NULL)
+        ErrExit("malloc");
+
+    read_file(inputBuffer, path_receiver, fileSize);
+    message_number = count_messages(inputBuffer, fileSize);
+    pid_struct *receiver_messages = parse_pid_struct(inputBuffer, message_number);
+    free(inputBuffer);
+//    handle_signals(sender_messages, receiver_messages, messages, message_number);
 
     free(messages);
+    free(sender_messages);
+    free(receiver_messages);
     return 0;
 }
